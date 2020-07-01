9e418c6f4e85e8138b310656822a2b03a9c2c21c
Hamilton Perez <hambone124@users.noreply.github.com>
2020-07-01 16:57:32 -0400

    Initial commit

---
+++ ShopifyProduct.js
@@ -0,0 +1,41 @@
+async function Get_ShopifyProductMetafield(
+    {
+        $ShopifyDomain,
+        $ShopifyAccessToken,
+        $ShopifyProductId,
+        $MetafieldNamespace,
+        $MetafieldKey
+    }
+) {
+    const $Query = `{
+        product(
+          id:"gid://shopify/Product/${$ShopifyProductId}"
+        ) {
+          metafield(
+            namespace: "${$MetafieldNamespace}"
+            key: "${$MetafieldKey}"
+          ) {
+            key
+            value
+          }
+        }
+      }`
+    const $Url = `https://${$ShopifyDomain}.myshopify.com/admin/api/2020-04/graphql.json`
+    const $Options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/graphql',
+            'X-Shopify-Access-Token': `${$ShopifyAccessToken}`,
+        },
+        body: $Query
+    }
+
+    try {
+        const $Product = await fetch($Url, $Options).then($Response => $Response.json())
+        return $Product.data.product.metafield
+    } catch (e) {
+        return {
+            error: e
+        }
+    }
+}
